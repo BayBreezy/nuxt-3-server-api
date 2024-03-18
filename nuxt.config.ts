c1986ac8fb23364efb9de74c2a96281f1ab70132
BayBreezy <behon.baker@yahoo.com>
2024-03-18 05:02:38 -0500

    chore: enable devtools & disable server logs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,26 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@pinia/nuxt"],
-	// custom tailwindcss path
-	tailwindcss: {
-		cssPath: "~/assets/main.css",
-	},
-	// server config variable
-	runtimeConfig: {
-		MONGO_URI: process.env.MONGO_URI,
-	},
-	// register nitro plugin
-	nitro: {
-		plugins: ["@/server/db/index.ts"],
-	},
-	/// transpile  afew packages
-	build: {
-		transpile: ["@headlessui/vue", "vue-toastification", "@headlessui/tailwindcss"],
-	},
+  // enable devtools
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@pinia/nuxt"],
+  // custom tailwindcss path
+  tailwindcss: {
+    cssPath: "~/assets/main.css",
+  },
+  // server config variable
+  runtimeConfig: {
+    MONGO_URI: process.env.MONGO_URI,
+  },
+  // register nitro plugin
+  nitro: {
+    plugins: ["@/server/db/index.ts"],
+  },
+  /// transpile  afew packages
+  build: {
+    transpile: ["@headlessui/vue", "vue-toastification", "@headlessui/tailwindcss"],
+  },
+  // Since nuxt v3.11, the nitro plugin is throwing errors. This is a workaround
+  features: {
+    devLogs: false,
+  },
 });
