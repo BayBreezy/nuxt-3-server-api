b8a8611e29a0fd5e84f4fe53fe74a5f71be78108
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-08-18 18:46:03 -0500

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,28 @@
 export default defineNuxtConfig({
   // enable devtools
   devtools: { enabled: true },
+
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@pinia/nuxt"],
+
   // custom tailwindcss path
   tailwindcss: {
     cssPath: "~/assets/main.css",
   },
+
   // server config variable
   runtimeConfig: {
     MONGO_URI: process.env.MONGO_URI,
   },
+
   // register nitro plugin
   nitro: {
     plugins: ["@/server/db/index.ts"],
   },
+
   /// transpile  afew packages
   build: {
     transpile: ["@headlessui/vue", "vue-toastification", "@headlessui/tailwindcss"],
   },
+
+  compatibilityDate: "2024-08-18",
 });
